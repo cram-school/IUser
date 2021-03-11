0f4596a440a43af451069807cdda5a358db4c156
tnlou <tnlou@justhealth.cn>
2021-03-11 16:59:21 +0800

    init

--- utils/database.js
+++ utils/database.js
@@ -1,22 +1,7 @@
-// const mysql = require('mysql')
-
+const mysql = require('mysql')
+const config = require('../constant/mysql.config')
 // // 创建数据池
-// const pool  = mysql.createPool({
-//   host     : '127.0.0.1',   // 数据库地址
-//   user     : 'root',    // 数据库用户
-//   password : '123456'   // 数据库密码
-//   database : 'my_database'  // 选中数据库
-// })
+export const pool  = mysql.createPool({
+  ...config
+})
  
-// // 在数据池中进行会话操作
-// pool.getConnection(function(err, connection) {
-   
-//   connection.query('SELECT * FROM my_table',  (error, results, fields) => {
-    
-//     // 结束会话
-//     connection.release();
- 
-//     // 如果有错误就抛出
-//     if (error) throw error;
-//   })
-// })
